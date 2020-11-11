bff175a57f6b2c3a2102ddb1c315ca3ce485f166
Kaps Vembian <tholkappiyan.vembian@mclaren.com>
2020-11-11 14:13:35 +0000

    Added POW-OPERATOR

--- ws01_ex01_st01.py
+++ ws01_ex01_st01.py
@@ -10,6 +10,8 @@ elif opn == "x":
     result = opr1 * opr2
 elif opn == "/":
     result = opr1 / opr2
+elif opn == "^":
+    result = opr1 ** opr2
 else:
     print("Invalid operation")
 print(str(opr1) + opn + str(opr2) + " = " + str(result))
